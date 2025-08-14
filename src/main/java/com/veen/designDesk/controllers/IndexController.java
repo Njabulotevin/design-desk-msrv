9ec10988e24ecb29ea498f399996d6a070176871
NJABULO30 <njabulotevinm@discovery.co.za>
2025-08-15 00:34:53 +0200

    add application details and config

--- src/main/java/com/veen/designDesk/controllers/IndexController.java
+++ src/main/java/com/veen/designDesk/controllers/IndexController.java
@@ -1,13 +1,35 @@
 package com.veen.designDesk.controllers;
 
+import com.veen.designDesk.configs.ApplicationConfigRepository;
+import com.veen.designDesk.configs.ConfigurationClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class IndexController {
 
+    @Value("${spring.application.name}")
+    private String APPLICATION_NAME;
+
+    @Value("${spring.profiles.active}")
+    private String PROFILE;
+
+    @Autowired
+    private ConfigurationClientService configurationClientService;
+
+
+
     @GetMapping("/")
-    public String index() {
-        return "Design Desk MSRV version 1.0.0";
+    public Map<String, String> index() {
+        return Map.of(
+                "applicationName", this.APPLICATION_NAME,
+                "version", configurationClientService.getConfig("app.msrv.version").getValue() ,
+                "description", "SaaS platform for managing graphic design and printing projects",
+                "environment", PROFILE.equalsIgnoreCase("dev") ? "Test" : "Production"
+        );
     }
 }
